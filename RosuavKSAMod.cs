4871c2acb6b4f44cef3c143a286fe65674e78cbc
Chris Angelico <rosuav@gmail.com>
2026-09-04 22:12:57 +1000

    Implement the basics of event hooks

--- RosuavKSAMod.cs
+++ RosuavKSAMod.cs
@@ -6,11 +6,16 @@
 //> import: KSA
 //> import: System.Private.CoreLib
 //> import: System.Runtime
+//> import: 0Harmony
+//> import: Brutal.Glfw
+//> import: Planet.Render.Core
 using StarMap.API;
 using KSA;
 using System;
 using System.IO;
 using System.Text;
+using HarmonyLib;
+using Brutal.GlfwApi;
 
 namespace RosuavKSAMod {
 	[StarMapMod]
@@ -27,6 +32,41 @@ namespace RosuavKSAMod {
 		[StarMapImmediateLoad]
 		public void Init(Mod definingMod) {
 			log("Hello, world");
+		}
+
+		[StarMapAllModsLoaded]
+		public void fullyLoaded() {Patcher.patch();}
+		[StarMapUnload]
+		public void unload() {Patcher.unload();}
+	}
+
+	[HarmonyPatch]
+	internal static class Patcher
+	{
+		private static Harmony m_harmony = new Harmony("RosuavKSAMod");
+		public static void patch() {
+			m_harmony?.PatchAll(typeof(Patcher).Assembly);
+		}
+
+		public static void unload() {
+			m_harmony?.UnpatchAll("SimpleMod");
+			m_harmony = null;
+		}
+
+		static void log(string msg) {
+			using (FileStream fs = File.Open("rosuav.log", FileMode.Append, FileAccess.Write, FileShare.ReadWrite)) {
+				Byte[] data = new UTF8Encoding(true).GetBytes(msg);
+				fs.Write(data, 0, data.Length);
+				Byte[] nl = {10};
+				fs.Write(nl, 0, 1);
+			}
+		}
+
+		[HarmonyPatch(typeof(Vehicle), nameof(Vehicle.OnKey))]
+		[HarmonyPrefix]
+		public static bool onKey(Vehicle __instance, RenderCore.Input.GlfwKeyEvent keyEvent) {
+			log("GOT A KEYBOARD");
+			return true;
 		}
 	}
 }
